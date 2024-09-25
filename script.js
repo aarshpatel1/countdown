fc9bcdb453ce62e7a28a31866354babf2d3268a9
Aarsh Patel <aarsh18patel@gmail.com>
2024-09-25 16:24:11 +0530

    initial commit

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let countdown = parseInt(counting.innerText);
 var start, pause, reset
 
 startBtn.on("click", function () {
+
+    let customTime = document.querySelector("#countdown").value
+    if (customTime) {
+        console.log(customTime)
+        countdown = customTime + 1
+    }
     start = setInterval(() => {
         if (countdown > 0) {
             countdown -= 1
