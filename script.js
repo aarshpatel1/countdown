c8ae591d63decca4f02d80c11a8861b89dbfaf54
Aarsh Patel <aarsh18patel@gmail.com>
2024-09-25 15:50:22 +0530

    now start button is working

---
+++ script.js
@@ -0,0 +1,24 @@
+const counting = document.querySelector("#counting")
+const startBtn = $("#startBtn");
+const pauseBtn = $("#pauseBtn");
+const resetBtn = $("#resetBtn");
+
+let countdown = parseInt(counting.innerText);
+var start, pause, reset
+
+startBtn.on("click", function () {
+    start = setInterval(() => {
+        if (countdown > 0) {
+            countdown -= 1
+            updateCounter(countdown)
+        } else {
+            // alert("Time Out")
+            clearInterval(start)
+            updateCounter("Time Out")
+        }
+    }, 1000);
+})
+
+function updateCounter(currentCount) {
+    counting.innerText = currentCount
+}
